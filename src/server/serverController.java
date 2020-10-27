9b1781913f813c4d8ac1f4733b7c9cc81d62d436
faulknert <faulknert@msoe.edu>
2020-10-26 23:21:16 -0500

    Created Server UI

---
+++ src/server/serverController.java
@@ -0,0 +1,25 @@
+package server;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.GridPane;
+
+import java.awt.*;
+
+public class serverController {
+
+    @FXML
+    private GridPane playerList;
+
+    @FXML
+    private Label serverStatus;
+
+    @FXML
+    private void start(){
+        
+    }
+
+    @FXML
+    private void close(){
+
+    }
+}
